beba7525e5c24a88f63017b1bdda5645e1547fba
Thomas Bloor <tbsliver@shadow.cat>
2018-01-25 12:16:54 +0000

    Quick modification to have a no-civi version

--- Vagrant/installwp.sh
+++ Vagrant/installwp.sh
@@ -1,19 +1,8 @@
 #! /bin/bash
 
-CIVI_VER="4.7.27"
-
 cd /home/ubuntu
 
-echo "Getting CiviCRM Install Files"
-wget --quiet https://download.civicrm.org/civicrm-$CIVI_VER-wordpress.zip
-wget --quiet https://download.civicrm.org/civicrm-$CIVI_VER-l10n.tar.gz
-
 wp core download --path=wordpress --locale=en_GB
-
-unzip -qq civicrm-$CIVI_VER-wordpress.zip \
-  -d /home/ubuntu/wordpress/wp-content/plugins \
-
-patch /home/ubuntu/wordpress/wp-content/plugins/civicrm/wp-cli/civicrm.php /vagrant/civicrm.php.patch
 
 cp /vagrant/wp-cli-config.yml /home/ubuntu/wp-cli.yml
 
@@ -27,18 +16,5 @@ wp core install \
   --admin_password=admin \
   --admin_email="ubuntu@example.com" \
   --skip-email
-
-wp civicrm install \
-  --dbhost=localhost \
-  --dbname=civicrm \
-  --dbuser=root \
-  --dbpass=abc123 \
-  --lang=en_GB \
-  --langtarfile=/home/ubuntu/civicrm-$CIVI_VER-l10n.tar.gz \
-  --site_url=192.168.34.11 \
-  --ssl=off \
-  --user=admin
-
-(crontab -l 2>/dev/null; echo "*/5 * * * * cd /home/ubuntu/wordpress && wp --user=admin civicrm api job.execute auth=0") | crontab -
 
 exit 0
